d05ac85b4758a957c906d043dc9a2b40e1ded8d4
LittleGalaxyrider <bartanics.flora@gmail.com>
2021-12-13 15:30:43 +0100

    creating ingredient model (learing the shortcut)
    -Q: why I always have to put 'undefined'?

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {
+  }
+}
+
+// !This can have the shortcut which is above!
+//
+//export class Ingredient {
+//  public name: string | undefined;
+//  public amount: number | undefined ;
+//
+//
+//constructor(name: string, amount: number) {
+//    this.name = name;
+//    this.amount = amount
+//  }
+//}
